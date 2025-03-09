482f9ef05d8121e5db6caf0d182e43892f672d2d
hadjuse <rabearimananahadjpro@gmail.com>
2025-03-09 23:36:01 +0100

    fix #4, fix #3 add index, cluster and tables

--- cluster_script/admin_cluster_tablespace_index.sql
+++ cluster_script/admin_cluster_tablespace_index.sql
@@ -1,4 +1,12 @@
 ALTER USER c##admin_cluster quota unlimited on users;
+ALTER USER C##admin_cluster QUOTA UNLIMITED ON TS_DONNEES;
+ALTER USER C##admin_cluster QUOTA UNLIMITED ON TS_INDEX;
+ALTER USER C##admin_cluster QUOTA UNLIMITED ON TS_LOGS;
+ALTER USER C##admin_cluster QUOTA UNLIMITED ON TS_SITE_CERGY;
+ALTER USER C##admin_cluster QUOTA UNLIMITED ON TS_SITE_PAU;
+SELECT TS_DONNEES FROM dba_tablespaces;
+SHOW CON_NAME;
+ALTER SESSION SET CONTAINER = XEPDB1;
 -- Je crée le user admin cluser
 -- Créer le user admin en tant que system
 DROP USER c##admin_cluster;
@@ -10,9 +18,13 @@ CREATE ROLE c##role_admin_cluster;
 GRANT CONNECT TO c##role_admin_cluster;
 GRANT RESOURCE TO c##role_admin_cluster;
 GRANT CREATE TABLESPACE TO c##role_admin_cluster;
+GRANT CREATE ANY TABLE TO c##role_admin_cluster;
 GRANT DROP TABLESPACE TO c##role_admin_cluster;
 GRANT ALTER TABLESPACE TO c##role_admin_cluster;
 GRANT CREATE ANY DIRECTORY TO c##role_admin_cluster;
+GRANT ALTER SESSION TO c##role_admin_cluster;
+GRANT SET CONTAINER TO c##role_admin_cluster;
+GRANT CREATE CLUSTER TO c##role_admin_cluste;
 GRANT c##role_admin_cluster TO c##admin_cluster;
 
 
