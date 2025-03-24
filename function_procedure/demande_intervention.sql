8408e71fd1ae0ee3c03406fc1f002df887bf0f0a
Sékou2109 <sekoubah2103@hotmail.com>
2025-03-24 17:05:03 +0100

    Creation des tables et insertions

---
+++ function_procedure/demande_intervention.sql
@@ -0,0 +1,11 @@
+-- A exécuter dans Oracle pour créer une demande d'intervention
+-- 1) Executer ce code
+-- 2) L'id de l'intervention créée sera affichée dans la console
+-- 3) Mettre à jour l'intervention avec le statut 'En_cours' ou 'Resolu' avec la procédure mettre_a_jour_intervention
+DECLARE v_id_intervention NUMBER;
+BEGIN
+    v_id_intervention := demande_intervention; -- Prend un matériel en panne au hasard
+    --v_id_intervention := demande_intervention(id_materiel); -- Prend un materiel spécifique
+    DBMS_OUTPUT.PUT_LINE('Intervention créée avec ID : ' || v_id_intervention);
+END;
+/
